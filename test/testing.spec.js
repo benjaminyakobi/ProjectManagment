f90f35473908b9f3f286236b1838e77afefca02e
ForSure-Sv <sagi6464@hotmail.com>
2020-04-16 09:55:48 +0300

    new bright day

--- test/testing.spec.js
+++ test/testing.spec.js
@@ -1,7 +1,7 @@
 const add = (x, y) => +x + +y
 
-const assert = require('assert')
+const assert = require("assert")
 
-it('correctly calculates the sum of 1 and 3', () => {
-  assert.equal(add(1, 3), 4)
+it("correctly calculates the sum of 1 and 3", () => {
+	assert.equal(add(1, 3), 4)
 })
