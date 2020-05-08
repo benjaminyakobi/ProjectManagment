ffe8f6fbf73aec59e4ca7c47f821959c8a2727cc
ForSure-Sv <sagi6464@hotmail.com>
2020-05-08 15:06:26 +0300

    funtcions

---
+++ functions/index.js
@@ -0,0 +1,69 @@
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+    //  response.send("Hello from Firebase!");
+    // });
+    
+    //var http = require('http');
+    //var fs = require('fs');
+    
+    // The Firebase Admin SDK to access the Firebase Realtime Database.
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const firebaseApp = admin.initializeApp(
+    functions.config().admin
+);
+
+var express = require('express');
+//var todoController = require('./controller/tdController')
+
+//var app = express();
+console.log(__dirname);
+//app.use(express.static(__dirname));
+/*app.get('/',function (req, res) {
+    res.sendFile('./index.html');
+});*/
+
+//new user signup
+exports.SignUp = functions.auth.user().onCreate((user) =>{
+    return admin.firestore().collection('users').doc(user.uid)({
+        email: user.email,
+        studentId: [],
+    });
+});
+
+exports.userDeleted = functions.auth.user().onDelete((user) =>{
+    const id = admin.firestore().collection('users').doc(user.uid);
+    return id;
+});
+
+exports.addRequest = functions.https.onCall((data,context)=>{
+    if(!context.auth){
+        throw new functions.https.HttpsError(
+            'unauthenticated',
+            'by passed login'
+        );
+    }
+    return admin.firestore().collection('units').add({
+
+    });
+});
+
+//exports.app = functions.https.onRequest(app);
+
+
+/*
+var kk = 3000;
+var server = http.createServer(function (req, res) {
+    if (req.url == '/index.html' || req.url == '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+        myReadStream.pipe(res);
+    }
+
+}
+)
+server.listen(kk, '127.0.0.1');
+console.log('sssddd');*/
--- src/app.js
+++ src/app.js
@@ -15,26 +15,26 @@ function initApp()
 
     // When the user clicks the button, open the modal 
     forgotPasswordField.onclick = function() {
-    modal.style.display = "block";
-    }
+        modal.style.display = "block";
+        }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-    modal.style.display = "none";
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
     }
 
     document.getElementById("getQuestion").innerHTML = getQuestionByUsername();
     function getQuestionByUsername(){
-    return 'your question';
+        return 'your question';
     }
 
     function verifyAnswer(){
-    return 'correct';
+        return 'correct';
     }
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-    if (event.target == modal) {
-    modal.style.display = "none";
+        if (event.target == modal) {
+            modal.style.display = "none";
     }
     }
 
