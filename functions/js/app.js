1af3547d860f92b4b315f3fb9cb1c49c820014f7
benjamin <benjaminyakobi@gmail.com>
2020-05-12 21:22:41 +0300

    upgraded view & registeration before fix

--- functions/js/app.js
+++ functions/js/app.js
@@ -36,7 +36,8 @@ function initApp()
         modal.style.display = "none";
     }
 
-    const signOutButton = document.getElementById("logoutButton");
+    // const signOutButton = document.getElementById("logoutButton");
+    const signOutButton = document.getElementById("createUser");
 
     const checkUsername = document.getElementById("checkUsernameBtn");
 
@@ -189,13 +190,15 @@ function initApp()
                 .then(() => {
                     if(val == "student"){
                         // eslint-disable-next-line promise/always-return
-                        alert("Successfully Registered as Student!");
+                        alert("Successfully Registered as a Student!");
+                        // window.location.href="/";
                         // location.reload();
                         // console.log("registered");
                     }
                     else if(val == "renter")
                     {
-                        alert("Successfully Registered as Renter!");
+                        alert("Successfully Registered as a Renter!");
+                        // window.location.href="/";
                         // console.log("registered renter");
                         // registerUserForm.reset();
                     }
